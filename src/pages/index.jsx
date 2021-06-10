29d46fbaf14ae8a7b8f0da1ec63055ff3a4abae3
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-06-09 21:36:24 -0300

    refactor: moved home page folder to page root to match next router path

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,54 @@
-import About from './About'
-import Contact from './Contact'
-import Database from './Database'
-import Detail from './Detail'
-import Home from './Home'
-import NotFound from './NotFound'
-import Pokedex from './Pokedex'
-import Types from './Types'
-import Battle from './Battle'
+import Head from 'next/head'
+import * as S from '../styles/home'
+import {useTheme} from 'styled-components'
+import pokemonVideo from 'assets/videos/pokemon-opening-season1.webm'
+import {useEffect} from 'react'
+import threePokemons from 'assets/images/three-pokemons.png'
+import mewTwo from 'assets/images/mewtwo.png'
 
-export {
-  About,
-  Contact,
-  Database,
-  Detail,
-  Home,
-  NotFound,
-  Pokedex,
-  Types,
-  Battle
+const Home = () => {
+  const theme = useTheme()
+
+  useEffect(() => {
+    theme.dispatch({
+      type: 'set',
+      prop: {
+        containerFull: false,
+        containerPadding: 0
+      }
+    })
+    return () => {
+      theme.dispatch({
+        type: 'set',
+        prop: {
+          containerFull: true,
+          containerPadding: '50px 0'
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <S.Container>
+      <Head>
+        <title>Pokedex</title>
+        <meta name='description' content='Best Pokedex of the world!' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <S.Video autoPlay loop muted src={pokemonVideo} />
+      <S.Shadow />
+      <S.Content>
+        <S.TitleTop align='start'>The nostalgic Pokemon is back...</S.TitleTop>
+        <S.TitleBottom align='end'>
+          See the infinite list and choose your Pokemon
+        </S.TitleBottom>
+        <S.Image src={threePokemons} />
+        <S.ImageMewTwo src={mewTwo} />
+        <S.MewTwoPower />
+      </S.Content>
+      <S.Arrow />
+    </S.Container>
+  )
 }
-export * from './Database'
-export * from './Detail'
-export * from './Home'
-export * from './Pokedex'
-export * from './Battle'
+
+export default Home
